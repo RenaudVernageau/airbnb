1cf079544b5ffb144ba4e76d14f3b8be5b9e5ef0
RenaudVernageau <renaudvrng@gmail.com>
2022-11-19 18:36:38 +0100

    Fix bug vercel index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
-import Header from '../components/header'
+import Header from '../components/Header'
 import MediumCard from '../components/MediumCard'
 import SmallCard from '../components/SmallCard'
 import LargeCard from '../components/LargeCard'
